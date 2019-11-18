93b494a256f24655b2539c6000b31284084228ba
Bitcoin Fast <57894265+BitcoinFast@users.noreply.github.com>
2019-11-18 21:49:25 +0530

    Update token.sol

--- token.sol
+++ token.sol
@@ -1,96 +1,173 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.13;
 
-contract Token {
+contract ERC20Basic {
+  uint256 public totalSupply;
+  function balanceOf(address who) public constant returns (uint256);
+  function transfer(address to, uint256 value) public returns (bool);
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
 
-    function totalSupply() constant returns (uint256 supply) {}
+contract ERC20 is ERC20Basic {
+  function allowance(address owner, address spender) public constant returns (uint256);
+  function transferFrom(address from, address to, uint256 value) public returns (bool);
+  function approve(address spender, uint256 value) public returns (bool);
+  event Approval(address indexed owner, address indexed spender, uint256 value);
+}
 
-    function balanceOf(address _owner) constant returns (uint256 balance) {}
+contract Ownable {
+  address public owner;
 
-    function transfer(address _to, uint256 _value) returns (bool success) {}
+  event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {}
+  function Ownable() {
+    owner = msg.sender;
+  }
 
-    function approve(address _spender, uint256 _value) returns (bool success) {}
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {}
+  /**
+   * @dev Throws if called by any account other than the owner.
+   */
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
 
-    event Transfer(address indexed _from, address indexed _to, uint256 _value);
-    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+  function transferOwnership(address newOwner) onlyOwner public {
+    require(newOwner != address(0));
+    OwnershipTransferred(owner, newOwner);
+    owner = newOwner;
+  }
+}
+
+library SaferMath {
+  function mulX(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a * b;
+    assert(a == 0 || c / a == b);
+    return c;
+  }
+
+  function divX(uint256 a, uint256 b) internal constant returns (uint256) {
+    // assert(b > 0); // Solidity automatically throws when dividing by 0
+    uint256 c = a / b;
+    // assert(a == b * c + a % b); // There is no case in which this doesn't hold
+    return c;
+  }
+
+  function sub(uint256 a, uint256 b) internal constant returns (uint256) {
+    assert(b <= a);
+    return a - b;
+  }
+
+  function add(uint256 a, uint256 b) internal constant returns (uint256) {
+    uint256 c = a + b;
+    assert(c >= a);
+    return c;
+  }
+}
+
+contract BasicToken is ERC20Basic {
+  using SaferMath for uint256;
+  mapping(address => uint256) balances;
+
+  function transfer(address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+
+    balances[msg.sender] = balances[msg.sender].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    Transfer(msg.sender, _to, _value);
+    return true;
+  }
+
+  function balanceOf(address _owner) public constant returns (uint256 balance) {
+    return balances[_owner];
+  }
+
+}
+
+contract StandardToken is ERC20, BasicToken {
+
+  mapping (address => mapping (address => uint256)) allowed;
+
+  function transferFrom(address _from, address _to, uint256 _value) public returns (bool) {
+    require(_to != address(0));
+
+    uint256 _allowance = allowed[_from][msg.sender];
+
+    balances[_from] = balances[_from].sub(_value);
+    balances[_to] = balances[_to].add(_value);
+    allowed[_from][msg.sender] = _allowance.sub(_value);
+    Transfer(_from, _to, _value);
+    return true;
+  }
+
+  function approve(address _spender, uint256 _value) public returns (bool) {
+    allowed[msg.sender][_spender] = _value;
+    Approval(msg.sender, _spender, _value);
+    return true;
+  }
+
+  function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
+    return allowed[_owner][_spender];
+  }
+
+  function increaseApproval (address _spender, uint _addedValue) returns (bool success) {
+    allowed[msg.sender][_spender] = allowed[msg.sender][_spender].add(_addedValue);
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+
+  function decreaseApproval (address _spender, uint _subtractedValue) returns (bool success) {
+    uint oldValue = allowed[msg.sender][_spender];
+    if (_subtractedValue > oldValue) {
+      allowed[msg.sender][_spender] = 0;
+    } else {
+      allowed[msg.sender][_spender] = oldValue.sub(_subtractedValue);
+    }
+    Approval(msg.sender, _spender, allowed[msg.sender][_spender]);
+    return true;
+  }
+}
+
+contract BitcoinFast is StandardToken, Ownable {
+
+  string public constant name = "Bitcoin Fast";
+  string public constant symbol = "BTCF";
+  uint8 public constant decimals = 8;
+
+  uint256 public constant SUPPLY_CAP = 21000000 * (10 ** uint256(decimals));
+
+  address NULL_ADDRESS = address(0);
+
+
+  event NoteChanged(string newNote);
+  string public note = "BITCOIN FAST";
+  function setNote(string note_) public onlyOwner {
+      note = note_;
+      NoteChanged(note);
+  }
+
+  
+  event PerformingDrop(uint count);
+  function drop(address[] addresses, uint256 amount) public onlyOwner {
+    uint256 amt = amount * 10**8;
+    require(amt > 0);
+    require(amt <= SUPPLY_CAP);
+    PerformingDrop(addresses.length);
     
-}
-
-
-
-contract StandardToken is Token {
-
-    function transfer(address _to, uint256 _value) returns (bool success) {
-
-        if (balances[msg.sender] >= _value && _value > 0) {
-            balances[msg.sender] -= _value;
-            balances[_to] += _value;
-            Transfer(msg.sender, _to, _value);
-            return true;
-        } else { return false; }
+    // Maximum drop is 1000 addresses
+    assert(addresses.length <= 1000);
+    assert(balances[owner] >= amt * addresses.length);
+    for (uint i = 0; i < addresses.length; i++) {
+      address recipient = addresses[i];
+      if(recipient != NULL_ADDRESS) {
+        balances[owner] -= amt;
+        balances[recipient] += amt;
+        Transfer(owner, recipient, amt);
+      }
     }
+  }
 
-    function transferFrom(address _from, address _to, uint256 _value) returns (bool success) {
-        if (balances[_from] >= _value && allowed[_from][msg.sender] >= _value && _value > 0) {
-            balances[_to] += _value;
-            balances[_from] -= _value;
-            allowed[_from][msg.sender] -= _value;
-            Transfer(_from, _to, _value);
-            return true;
-        } else { return false; }
-    }
-
-    function balanceOf(address _owner) constant returns (uint256 balance) {
-        return balances[_owner];
-    }
-
-    function approve(address _spender, uint256 _value) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-        return true;
-    }
-
-    function allowance(address _owner, address _spender) constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
-    }
-
-    mapping (address => uint256) balances;
-    mapping (address => mapping (address => uint256)) allowed;
-    uint256 public totalSupply;
-}
-
-
-contract BitcoinFast is StandardToken {
-
-    function () {
-
-        throw;
-    }
-
-    string public name;   
-    uint8 public decimals;
-    string public symbol; 
-    string public version = '1.0';
-
-
-    function BitcoinFast(
-        ) {
-        balances[msg.sender] = 21000000;        
-        totalSupply = 21000000;                 
-        name = "Bitcoin Fast";                  
-        decimals = 8;                           
-        symbol = "BTCF";                              
-    }
-
-
-    function approveAndCall(address _spender, uint256 _value, bytes _extraData) returns (bool success) {
-        allowed[msg.sender][_spender] = _value;
-        Approval(msg.sender, _spender, _value);
-
-       
-        if(!_spender.call(bytes4(bytes32(sha3("receiveApproval(address,uint256,address,bytes)"))), msg.sender, _value, this, _extraData)) { throw; }
-        return true;
-    }
+  function BitcoinFast() {
+    totalSupply = SUPPLY_CAP;
+    balances[msg.sender] = SUPPLY_CAP;
+  }
 }
